c71fb01f800e399de0d837a4221137c7d9bdf64e
Jennifer <missjenab@hotmail.com>
2021-09-05 22:04:55 -0400

    final

--- smaller.cpp
+++ smaller.cpp
@@ -2,31 +2,30 @@
 Author: Jennifer Baranyai
 Course: CSCI-135
 Instructor: Tong Yi and Mike Zamansky
-Assignment: Lab 1 A
+Assignment: Lab 1A
 
 A program that asks the user to input two integer numbers and
 prints out the smaller of the two.
 */
 
 #include <iostream>
-using namespace std;
 
 int main()
 {
    int first, second, smaller;
-   cout << "Enter the first number: ";
-   cin >> first;
-   cout << "Enter the second number: ";
-   cin >> second;
+   std::cout << "Enter the first number: ";
+   std::cin >> first;
+   std::cout << "Enter the second number: ";
+   std::cin >> second;
    if (first < second)
    {
       smaller = first;
-      cout << endl << "The smaller of the two is " << smaller;
+      std::cout << "\nThe smaller of the two is " << smaller;
    }
    else
    {
       smaller = second;
-      cout << endl << "The smaller of the two is " << smaller;
+      std::cout << "\nThe smaller of the two is " << smaller;
    }
    return 0;
 }
